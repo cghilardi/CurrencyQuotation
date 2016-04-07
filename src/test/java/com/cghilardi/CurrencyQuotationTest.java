19365b865cde92c4aa1bc340b958ddcf0c7d86c4
cghilardi <cassioghilardi@hotmail.com>
2016-04-07 03:28:58 -0300

    Adds test to validate exception is raised when value < 0

--- src/test/java/com/cghilardi/CurrencyQuotationTest.java
+++ src/test/java/com/cghilardi/CurrencyQuotationTest.java
@@ -7,26 +7,30 @@ import static org.mockito.Mockito.*;
 import java.math.BigDecimal;
 
 public class CurrencyQuotationTest {
-
     private CurrencyQuotation currencyQuotation;
-    private String quotation, formattedQuotation, from, to;
+    private String quotation, from, to;
     private TaxStorage taxStorage;
 
     @Before
     public void setUp() {
         quotation = "11/11/2010";
-        formattedQuotation = CurrencyQuotation.format(quotation);
         from = "USD";
         to = "ARS";
         taxStorage = mock(TaxStorage.class);
         currencyQuotation = new CurrencyQuotation(taxStorage);
-        when(taxStorage.get(formattedQuotation, from)).thenReturn(new BigDecimal(4));
-        when(taxStorage.get(formattedQuotation, to)).thenReturn(new BigDecimal(0.25));
+        
+        when(taxStorage.get(any(String.class), eq(from))).thenReturn(new BigDecimal(4));
+        when(taxStorage.get(any(String.class), eq(to))).thenReturn(new BigDecimal(0.25));
     }
 
     @Test
     public void convertToTheProperAmount() {
         assertEquals(new BigDecimal(160).setScale(2), currencyQuotation.currencyQuotation(from, to, 10, quotation));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void raiseExceptionWhenValueIsNegative() {
+        currencyQuotation.currencyQuotation(from, to, -1, quotation);
     }
 
     @Test
